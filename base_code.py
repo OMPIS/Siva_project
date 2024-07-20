69b610c2485501a70eaface625a7dc51a5de09b9
k!ck <OMPIS@users.noreply.github.com>
2024-07-20 21:45:32 +0530

    Update base_code.py

--- base_code.py
+++ base_code.py
@@ -14,10 +14,6 @@ def Hardware_details(cmd):
     return output
 
 
-manufacture = Hardware_details("Manufacturer | awk -F: '{print $2}'")
-product = Hardware_details("'Product Name' | awk -F: '{print $2}'")
-
-
 def DELL_iDRAC(version, verion_req):
     idrack_version = os.popen("racadm getversion | grep iDRAC | awk -F= '{print $2}'").read().strip()
     if idrack_version.startswith(verion_req):
@@ -33,11 +29,16 @@ def DELL_MEGACLI_ID(cmd):
     unique_ids = list(set(id_output.split()))
     return unique_ids
 
+def DELL_MEGACLI_ERROR_CHECKS(eid, slotid):
+    base_cmd_path = f"/opt/MegaRAID/MegaCli/ -PDInfo -PhysDrv[{eid}:{slotid}] -aALL"
+    error_counts, regex = ["Media Error Count", "Other Error Count"], "awk -F: '{print $2}'"
+    for error_count in error_counts:
+        validation = os.popen(f"{base_cmd_path} | grep '{error_count}' | {regex}").read().strip()
+        print(validation)
+    
 
-# DELL_iDRAC("PowerEdge R650", "7.00")
-# DELL_iDRAC("PowerEdge R720xd", "2.65")
-# DELL_iDRAC("PowerEdge R730xd", "2.80")
-# DELL_iDRAC("PowerEdge R740xd", "7.00")
+
+#Basic Varibles
 hwd_cmp = {
     "PowerEdge R650": "7.00",
     "PowerEdge R720xd": "2.65",
@@ -45,14 +46,21 @@ hwd_cmp = {
     "PowerEdge R740xd": "7.00"
 }
 
+manufacture = Hardware_details("Manufacturer | awk -F: '{print $2}'")
+product = Hardware_details("'Product Name' | awk -F: '{print $2}'")
+dell_enclouse_ids = DELL_MEGACLI_ID("Enclosure Device ID")
+dell_slot_ids = DELL_MEGACLI_ID("Slot Number")
+print(f"Dell slot id: {dell_slot_ids}")
+print(f"Dell enclouse id: {dell_slot_ids}")
+
 if os_name == "Linux":
     if "Dell" in manufacture:
         print(f"OS name: {os_name}")
         print(f"Manufacture: {manufacture}")
         if product.startswith("PowerEdge"):
             DELL_iDRAC(product, hwd_cmp[product])
-
-# dell_enclouse_id = DELL_MEGACLI_ID("Enclosure Device ID")
-# dell_slot_id = DELL_MEGACLI_ID("Slot Number")
-# print(f"Dell slot id: {dell_slot_id}")
-# print(f"Dell enclouse id: {dell_slot_id}")
+            for dell_enclouse_id in dell_enclouse_ids:
+                for dell_slot_id in dell_slot_ids:
+                    print(f"eid: {dell_enclouse_id}, sid: {dell_slot_id}")
+                    DELL_MEGACLI_ERROR_CHECKS(dell_enclouse_id, dell_slot_id)
+                    
